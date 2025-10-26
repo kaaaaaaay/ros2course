77199db9f38d6b11d038bb9f4e86bc708d9a8165
Kay <kay.engelmann@live.com>
2025-10-26 16:10:33 +0000

    Followed the tutorial until the II. The colcon build tool

---
+++ ch2-1-creating-nodes/ros2-node-test-workspace/src/ros2-sample-package-python/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2-sample-package-python'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Kay',
+    maintainer_email='k.engelmann@campus.tu-berlin.de',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'test_our_node = ros2-sample-package-python.sample_node:main',
+        ],
+    },
+)
